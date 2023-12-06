b0d2ef2a93804885b64d873ec2dc7329b2ad03f6
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2023-12-06 09:41:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // Treat all js code as newer version
+
+//alert (2+2); we are using node js, not browser
+
+console.log ( 2
+    +2)
+
+    console.log("Vishal"); // code readability is essential
+
+    let name = " Vishal"
+    let age = 23
+    let isLoggedIn = true
+
+    let state = null;
+
+    // number => 2 to power 53
+    // bigint
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined =>
+    //symbol => unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof undefined);
