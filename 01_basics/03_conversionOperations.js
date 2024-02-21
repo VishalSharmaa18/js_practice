8199d2924518e1190ce6938fdcc16cf9a1ba6222
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-02-21 18:11:35 +0000

    operations discussed

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,56 @@
+ let score = "33abc"
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//33 => 33
+//"33abc" => NaN
+//true => 1 , false => 0
+
+let isLoggedIn ="Vishal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+//1 => true, 0 => false
+// "" => false"
+// "Vishal" = > true
+
+let someNumber = 18
+
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber)
+
+
+// **************************** Operations ****************************
+
+// let Value = 3
+// let numValue =  -Value;
+// console.log(numValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello";
+// let str2 = " vishal";
+
+//  console.log(str1 +str2);
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" +2+3);
+// console.log(1 + 2 + "3");
+
+console.log(+true);
