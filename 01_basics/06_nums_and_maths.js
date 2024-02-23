d450a9d049f7fa4267065c91fd1f7e39b953560f
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-02-23 11:34:37 +0000

    Numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,8 @@
+const score = 500;
+console.log(score);
+
+const balance = new Number(3000000);
+console.log(balance);
+// console.log(balance.toExponential());
+console.log(balance.toFixed(2));
+console.log(balance.toLocaleString('en-IN'));
