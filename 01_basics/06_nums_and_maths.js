0ed4575b7376fff3c0fc0df36e23bd9a52312b99
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-02-23 17:21:06 +0000

    Number and Maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,8 +1,25 @@
 const score = 500;
 console.log(score);
 
-const balance = new Number(3000000);
-console.log(balance);
-// console.log(balance.toExponential());
-console.log(balance.toFixed(2));
-console.log(balance.toLocaleString('en-IN'));
+// const balance = new Number(3000000);
+// console.log(balance);
+// // console.log(balance.toExponential());
+// console.log(balance.toFixed(2));
+// console.log(balance.toLocaleString('en-IN'));
+
+// ************************ Maths **********************//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(6.7));
+// console.log(Math.sqrt(625));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+const max = 20;
+const min = 10;
+
+console.log(Math.random() * (max-min + 1) + min);
