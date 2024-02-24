9284afc661bb4badd1c33f67327a9b572e40722d
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-02-24 12:20:22 +0000

    string

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello there! My name is ${name} and my repoCount is ${repoCount}`);
 
-const gameName = new String ('Vishal Shamra')
+const gameName = new String ('Vishal Shamra us')
 console.log(gameName.__proto__);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3));
@@ -23,4 +23,12 @@ console.log(url.replace('%30' ,'-'))
 console.log(url.includes('umang'));
 
 console.log(gameName.split(''));
+
+const newValue = new String(
+    name = "Vishal sharma",
+    email = "Vishal@google.com"
+);
+
+console.log(`Hello there! my name is ${name} and my email is ${email}.. Thank You`);
+
 
