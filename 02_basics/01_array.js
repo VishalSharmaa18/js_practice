498026360d29e42909139702f4fd2c417f86e164
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-02-25 10:56:25 +0000

    Arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,43 @@
+// // const arr1 = [1,2,3,4,5];
+// // console.log(arr1[1]);
+
+// const myArr = new Array("Vishal","Umang","Sachin","Boat")
+// // console.log(myArr[2]);
+
+// // Array Methods
+
+// // console.log(myArr.push("20"));
+// // console.log(myArr);
+// console.log(myArr.pop());
+// console.log(myArr);
+// // myArr.shift();
+// // console.log(myArr);
+// myArr.unshift(10);
+// console.log(myArr);
+
+// const newArr = [10,20,30,40,50]
+// // console.log(newArr.includes(30));
+// // console.log(newArr.indexOf(50));
+// // console.log(newArr.join('-'));
+
+// // const myArrr = newArr.join('-');
+// // console.log( myArrr);
+// // console.log(newArr);
+
+const sportsName =  ["Cricket","Football","Volleyball","Hockey","Rugby"]
+// console.log(sportsName);
+
+// const newDefArr = sportsName.slice(0,2);
+// console.log("A : ",sportsName);
+// console.log(newDefArr);
+// console.log("B: ", sportsName);
+
+console.log("A : ",sportsName);
+const newDefArr = sportsName.splice(0,3);
+console.log("B : ",sportsName);
+ console.log(newDefArr);
+ console.log("c: ", sportsName);
+
+
+
+
