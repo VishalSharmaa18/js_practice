ff89f16ef3f58a070c61fa00d78c7c7177d1ec7c
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-03-09 09:37:59 +0000

    if else

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -17,3 +17,29 @@ else{
 }
 
 // <, > ,<=, >=,==, ===,!=,
+
+const balance = 1000;
+
+ if(balance >500){
+    console.log("Good");
+ }
+ else if(balance>750){
+  console.log("Very Good");
+ }
+ else if(balance>950){
+    console.log("Excellent");
+ }
+
+const isUserLoggedIn = true;
+const debircard = true;
+
+if(isUserLoggedIn && debircard && 5==5){
+   console.log("Ready to purchase course");
+}
+
+const isLoggedInFromGoogle = true;
+const isLoggedInFromEmail = false;
+
+if(isLoggedInFromGoogle || isLoggedInFromEmail){
+    console.log("Show Signin Button");
+}
