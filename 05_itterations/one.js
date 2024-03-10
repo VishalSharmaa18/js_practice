2950eca8161ab7aec939b2aa7f88564888bb22e7
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-03-10 11:02:42 +0000

    for loop

---
+++ 05_itterations/one.js
@@ -0,0 +1,11 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(i==5){
+//         console.log("5 is the best number");
+//     }
+//     console.log(element);
+// }
+
+for (let i = 0; i < array.length; i++) {
+    const element = i;    
+}
