2b0bb1931d8c0a1cb5d4c3a9e43c8d8ce1370e6f
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-03-11 11:09:10 +0000

    loops discussion

---
+++ 05_itterations/two.js
@@ -0,0 +1,20 @@
+ //while
+// let i = 0;
+// while (i<=10) {
+//     console.log(`Value of i is ${i}`);
+//     i = i+2;
+// }
+
+// const mySuperHeros = ["Rohit Sharma","MS Dhoni","Virat Kohli","Yashasvi Jaiswal"]
+// let myArr = 0;
+// while (myArr<=mySuperHeros.length ) {
+//     console.log(`The Real Superhero are ${mySuperHeros[myArr]}`);
+//     myArr = myArr +1;
+// }
+
+//Do-while
+let score = 1
+do {
+   console.log(`Value of score is ${score}`); 
+   score++;
+} while (score<=10);
