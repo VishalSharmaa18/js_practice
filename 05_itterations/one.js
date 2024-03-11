8e1f570410ff485f080a04c4502b31530ddcc98b
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-03-11 10:57:58 +0000

    for loop and break st

--- 05_itterations/one.js
+++ 05_itterations/one.js
@@ -6,6 +6,18 @@
 //     console.log(element);
 // }
 
-for (let i = 0; i < array.length; i++) {
-    const element = i;    
+// let myArray = ["Virat Kohli","Rohit Sharma","MS Dhoni"]
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(`The Real Superheros are ${element}`);    
+// }
+
+//break and continue
+
+for (let index = 0; index < 10; index++) {
+  if (index==5) {
+    console.log("5 is the best number");
+    break;
+  }
+  console.log(`Value of index is ${index}`);
 }
