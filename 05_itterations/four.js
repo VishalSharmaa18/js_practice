d767cbd1ea5a5057b6aa89adac5b61781fef1b61
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-03-12 15:31:03 +0000

    Loops Discussion

--- 05_itterations/four.js
+++ 05_itterations/four.js
@@ -6,5 +6,22 @@ const myObject = {
 }
 
 for (const key in myObject) {
-   console.log(key);
+ //  console.log(`${key} is shortcut for ${myObject[key]}`);
 }
+
+const programming = ["JS","CPP","RUBY","PY"]  
+
+for (const key in programming) {
+  //      console.log(programming[key]); 
+}
+
+// const map1 = new Map();
+//                               // No itterations on maps in forin loop
+// map1.set('IN','India')
+// map1.set('USA','United States of America');
+// map1.set('UAE','United Arab Emeriates');
+
+// for (const [key] in map1) {
+//    console.log(key);
+// }
+
