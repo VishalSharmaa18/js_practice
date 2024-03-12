87e4e70a01853f3d48c0f50878c5deb82324ba1f
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-03-12 17:11:06 +0000

    forEach

---
+++ 05_itterations/five.js
@@ -0,0 +1,15 @@
+const sports = ["Cricket","Football","Hockey","Basketball"];
+
+// sports.forEach(function (item) {
+// console.log(item);
+// })
+
+// sports.forEach(element => {
+//     console.log(element);
+// });
+
+function printMe(list) {
+    console.log(list);
+}
+
+sports.forEach(printMe)
