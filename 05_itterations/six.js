efa83ae1d29080194963d0c8ef9726a354776269
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-03-14 11:05:51 +0000

    Loops D

---
+++ 05_itterations/six.js
@@ -0,0 +1,8 @@
+// const sports = ["Cricket","Football","Hockey","Basketball"];
+
+// const values = sports.forEach((item)=>{
+//     console.log(item);
+//     return
+// })
+
+// console.log(values);
