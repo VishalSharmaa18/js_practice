5f2595f8c1e91217424977709417dfc2e67c41c5
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-04-24 10:01:59 +0000

    promises consumed

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,1 +1,19 @@
-const promiseOne = new Promise();
+const promiseOne = new Promise(function(resolve,reject){
+    //Do an async task
+    //DB calls,cryptography,network
+    setTimeout(function(){
+        console.log("Async task is completed");
+        resolve();
+    },1000)
+});
+
+promiseOne.then(()=>{console.log("promise consumed")});
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("async 2 completed");
+        resolve();
+    },2000)
+}).then(function(){
+    console.log("Task 2 completed")
+})
