3a3f3c68281998778307456bcdb5088f4b058aa1
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-04-24 10:48:31 +0000

    promise hell

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -13,7 +13,38 @@ new Promise(function(resolve,reject){
     setTimeout(function(){
         console.log("async 2 completed");
         resolve();
-    },2000)
+    },1000)
 }).then(function(){
     console.log("Task 2 completed")
-})
+});
+
+const promiseThree = new Promise(function(resolve,reject){
+   setTimeout(() => {
+    resolve({Username: "Vishal",email:"vishal@gmail.com"});
+   }, 1000);
+});
+promiseThree.then(function(user){
+console.log(user)
+});
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(() => {
+        const error = true;
+        if(!error){
+           resolve({username:"Vishal",password:"abc"})
+        }else{
+            reject('Error : Something went wrong')
+        }
+    }, 1000);
+});
+
+promiseFour.then((user)=>{
+console.log(user)
+return user.username
+}).then((myuserName)=>{
+    console.log(myuserName)
+}).catch((error)=>{
+    console.log(error)
+}).finally(()=>{
+    console.log("the promise is either resolved or rejected")
+});
