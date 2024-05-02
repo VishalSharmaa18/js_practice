c4ca52d4acc7695c2b9e0da677d958aac385cb17
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-05-02 11:23:28 +0000

    pou

--- 10_classes_and_oops/object.js
+++ 10_classes_and_oops/object.js
@@ -1,1 +1,13 @@
-console.log("Prototype");
+function multiplyBy5(num){
+return num * 5;
+}
+ 
+multiplyBy5.power = 3;
+
+console.log(multiplyBy5(5));
+console.log(multiplyBy5.power);
+console.log(multiplyBy5.prototype)
+
+function createUser (username,score){
+    this.username = username
+}
