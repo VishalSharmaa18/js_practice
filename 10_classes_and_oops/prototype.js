bb4f36183c1216965b8776a0dceb89802cc396dd
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-05-08 15:35:46 +0000

    prototype in javascript

--- 10_classes_and_oops/prototype.js
+++ 10_classes_and_oops/prototype.js
@@ -1,5 +1,72 @@
-let myName = "vishal   ";
-let myChannel = "driftwithvishal   ";
+// let myName = "vishal     ";
+// let channelName = "driftwithvishal  "
 
-console.log(myChannel.truelength);
+// console.log(myName.truelength);
+// console.log(channelName.trim()-length);
+
+const myHeros = ["Virat Kohli","Rohit Sharma","Chris Gayle"];
+
+const herosNickName = {
+    kohli : "Run-Machine",
+    rohit : "Hitman",
+    gayle : "Universal-Boss",
+
+    getNickName :function(){
+        console.log(`Chirs Gayle NickName is ${this.gayle}`)
+    }
+};
+
+Object.prototype.heyHeros = function(){
+    console.log("Hey Heros present in all")
+};
+
+herosNickName.heyHeros();
+myHeros.heyHeros();
+
+Array.prototype.heyArray = function(){
+    console.log("Hey it will partially present in all")
+}
+
+myHeros.heyArray();
+// herosNickName.heyArray(); it will not present in object
+
+//inheritance
+
+const User = {
+userName : "Vishal Sharma",
+email : 'VS@home.com'
+}
+
+const Teacher = {
+    makeVideo : true
+}
+
+const TeachingSupport = {
+    isAvailable : false
+}
+
+const TASupport = {
+makeAssignment : 'Js assignment',
+fullTime : true,
+__proto__ : TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+
+Object.setPrototypeOf(TeachingSupport,Teacher);
+
+let anotherUserName = "js With Vishal   ";
+
+String.prototype.truelength = function(){
+    console.log(`${this}`)
+    console.log(`True Length is : ${this.trim().length}`)
+}
+anotherUserName.truelength();
+
+"Vishal".truelength();
+
+
+
 
