962578a08883c1ea61ea630e5b1fc583cff61d95
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2024-05-08 15:50:43 +0000

    call keyword in javascript

---
+++ 10_classes_and_oops/call.js
@@ -0,0 +1,13 @@
+function setUserName(username){
+    //Complex db calls
+    this.username = username
+};
+
+function createUser(username,email,password){
+    setUserName.call(this,username);
+  this.email = email;
+  this.password = password;
+}
+
+const user = new createUser("Vishal","Vishal@home.com","123");
+console.log(user)
